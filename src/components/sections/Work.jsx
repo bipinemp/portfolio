c7057c0a51bb0cbcf7b28d87f00dba33ba1cd63e
bipinemp <bhandaribipin3.39@gmail.com>
2023-10-26 10:39:12 +0545

    changes

--- src/components/sections/Work.jsx
+++ src/components/sections/Work.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ecommerce from "../../assets/works/ecommerce.png";
-import tesla from "../../assets/works/tesla.png";
 import movie from "../../assets/works/movie.png";
 import { FiGithub } from "react-icons/fi";
 import { RiExternalLinkLine } from "react-icons/ri";
@@ -10,7 +9,6 @@ import { motion } from "framer-motion";
 function Work() {
   const { animation: animation1, ref: ref1 } = useAnimateHook(0.2);
   const { animation: animation2, ref: ref2 } = useAnimateHook(0.3);
-  const { animation: animation3, ref: ref3 } = useAnimateHook(0.4);
   return (
     <div className="work" id="work">
       <div className="work1">
@@ -67,8 +65,8 @@ function Work() {
         </motion.div>
 
         <motion.div
-          ref={ref3}
-          animate={animation3}
+          ref={ref2}
+          animate={animation2}
           className="work2-1 first-work"
         >
           <div className="work2-1-1">
@@ -102,50 +100,6 @@ function Work() {
               </div>
               <div>
                 <a href="https://flimbuzz.vercel.app/" target="_blank">
-                  <RiExternalLinkLine className="icon2" />
-                  <p title="Live Demo"></p>
-                </a>
-              </div>
-            </div>
-          </div>
-        </motion.div>
-        <motion.div
-          ref={ref2}
-          animate={animation2}
-          className="work2-1 second-work"
-        >
-          <div className="work2-1-1 work2-1-1-1">
-            <img src={tesla} alt="tesla website" loading="lazy" />
-          </div>
-          <div className="work2-1-2 work2-1-2-1-1">
-            <div className="work2-1-2-1">
-              <p>Featured Project</p>
-            </div>
-            <div className="work2-1-2-2">
-              <h1>Tesla Clone</h1>
-            </div>
-            <div className="work2-1-2-3 work2-1-2-3-1">
-              <p>
-                My Tesla website clone accurately replicates the official front
-                page of the official website.
-              </p>
-            </div>
-            <div className="work2-1-2-4-1">
-              <span>React</span>
-              <span>CSS</span>
-            </div>
-            <div className="work2-1-2-5">
-              <div>
-                <a
-                  href="https://github.com/bipinemp/Tesla_clone"
-                  target="_blank"
-                >
-                  <FiGithub className="icon1" />
-                  <p title="Github"></p>
-                </a>
-              </div>
-              <div>
-                <a href="https://reacttesla.netlify.app/" target="_blank">
                   <RiExternalLinkLine className="icon2" />
                   <p title="Live Demo"></p>
                 </a>
